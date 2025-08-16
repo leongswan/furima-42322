8d5bb62ba2346bec66fd68af5ef597b70dfdc1f3
leongswan <leongswan19860919@gmail.com>
2025-08-16 11:23:06 +0800

    chore: enforce LF via .gitattributes and renormalize

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,29 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save
+      redirect_to root_path, notice: '出品が完了しました。'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name, :description, :category_id, :condition_id, :delivery_fee_id,
+      :area_id, :shipping_day_id, :price, :image
+    )
   end
 end
