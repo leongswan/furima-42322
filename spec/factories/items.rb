d06033d0d203258a47256c3b614dd799e4d68b43
leongswan <leongswan19860919@gmail.com>
2025-08-23 16:00:23 +0900

    テスト中フリーズ対策修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,24 +1,25 @@
 FactoryBot.define do
   factory :item do
-    name            { 'テスト商品' }
-    description     { 'テスト説明' }
-    category_id     { 2 }
-    condition_id    { 2 }
+    name { 'テスト商品' }
+    description { 'テスト説明' }
+    category_id { 2 }
+    condition_id { 2 }
     delivery_fee_id { 2 }
-    area_id         { 2 }
+    area_id { 2 }
     shipping_day_id { 2 }
-    price           { 1000 }
+    price { 1000 }
     association :user
 
     after(:build) do |item|
       next if item.image.attached?
       path = Rails.root.join('spec/fixtures/files/leonbigcat_small.png')
-      uploaded_file = Rack::Test::UploadedFile.new(path, 'image/png')
-      item.image.attach(
-        io: uploaded_file,
-        filename: 'leonbigcat_small.png',
-        content_type: 'image/png'
-      )
+      File.open(path, 'rb') do |f|
+        item.image.attach(
+          io: f,
+          filename: 'leonbigcat_small.png',
+          content_type: 'image/png'
+        )
+      end
     end
   end
 end
