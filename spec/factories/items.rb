35ed1c8d44d21d742a0a336c67ef749768eaca70
leongswan <leongswan19860919@gmail.com>
2025-08-23 15:11:15 +0900

    内容の修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,26 +1,24 @@
 FactoryBot.define do
   factory :item do
-    name { 'テスト商品' }
-    description { 'テスト説明' }
-    category_id { 2 }
-    condition_id { 2 }
+    name            { 'テスト商品' }
+    description     { 'テスト説明' }
+    category_id     { 2 }
+    condition_id    { 2 }
     delivery_fee_id { 2 }
-    area_id { 2 }
+    area_id         { 2 }
     shipping_day_id { 2 }
-    price { 1000 }
+    price           { 1000 }
     association :user
 
     after(:build) do |item|
       next if item.image.attached?
-
       path = Rails.root.join('spec/fixtures/files/leonbigcat_small.png')
-      File.open(path) do |file|
+      uploaded_file = Rack::Test::UploadedFile.new(path, 'image/png')
       item.image.attach(
-        io: file,
+        io: uploaded_file,
         filename: 'leonbigcat_small.png',
         content_type: 'image/png'
       )
-      end
     end
   end
 end
